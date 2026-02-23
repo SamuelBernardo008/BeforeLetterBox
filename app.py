09d431472dc1493a346f7c0456ec1f954027543f
Aluno Técnico ADS2 <samuelbernardorodrigues2008@gmail.com>
2026-02-23 14:18:19 -0300

    fix: corrige lógica das funções de editar e deletar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from models.recomendacao import Recomendacao
 from models.database import init_db
 
@@ -24,3 +24,26 @@ def listadesejo():
     recomendacoes = Recomendacao.obter_recomendacoes()
 
     return render_template('ListaDesejo.html', titulo= 'Before Letter Box', recomendacoes=recomendacoes)
+
+
+@app.route('/delete/<int:idRecomendacao>')
+def delete(idRecomendacao):
+    recomendacao = Recomendacao.id(idRecomendacao)
+    recomendacao.excluir_recomendacao()
+    return redirect(url_for('listadesejo'))
+
+@app.route('/update/<int:idRecomendacao>' , methods=['GET', 'POST'])
+def update(idRecomendacao):
+
+    if request.method == 'POST':
+        titulo = request.form ['titulo-recomendacao']
+        quem = request.form['quem-recomendacao']
+        tipo = request.form['tipo-recomendacao']
+        recomendacao = Recomendacao(titulo, quem, tipo, idRecomendacao) 
+        recomendacao.atualizar_recomendacao()
+        return redirect(url_for('listadesejo'))
+    
+    recomendacoes = Recomendacao.obter_recomendacoes()
+    recomendacao_selecionada = Recomendacao.id(idRecomendacao)
+
+    return render_template('listadesejo.html', titulo= 'Lista de Desejos', recomendacoes=recomendacoes, recomendacao_selecionada=recomendacao_selecionada)
